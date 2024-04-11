160d947e3b33929ff35c25ba927f25f549c19a3b
Olaoluwa Mustapha <37044906+OlaoluwaM@users.noreply.github.com>
2024-04-11 13:40:48 -0500

    chore: update

--- app/main.hs
+++ app/main.hs
@@ -1,6 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+
 -- import Application (appMain)
-import HelloWorld
+-- import HelloWorld
 import Yesod
 
+data App = App
+
+mkYesod "App" [parseRoutes| / HomeR GET |]
+
+instance Yesod App
+
+getHomeR = return $ object ["msg" .= ("Hello World!" :: String)]
+
 main :: IO ()
-main = warp 3000 HelloWorld
+main = warp 3000 App
